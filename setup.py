cd889799da1ea295be3e9ab68ef775c890a7bce3
ejucovy <ethan.jucovy@gmail.com>
2013-10-21 16:28:08 -0400

    And gunicorn

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='pluggable-django-base',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-      "dj-database-url", "south",
+      "dj-database-url", "south", "gunicorn",
       
       ],
       entry_points="""  """,
